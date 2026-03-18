87c10c3ade468516987a900f42d905f0556de780
Batubo Victory <victorybatubo76@gmail.com>
2026-03-18 18:30:39 +0100

    fix: Change file type

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,2 +1,10 @@
 import express from 'express'
-const app = express()
+
+console.log('file is running')
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
