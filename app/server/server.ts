f6cbd07b5a4d1aafce69396fb2201d085763b306
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 16:19:13 +0100

    build: Finish config settings

---
+++ app/server/server.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: './app/server/.env' })
+
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' })
+})
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'ItIsWorkingNow' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
--- node_modules/@typescript-eslint/utils/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * as ASTUtils from '@typescript-eslint/utils/ast-utils';
-export * as ESLintUtils from '@typescript-eslint/utils/eslint-utils';
-export * as JSONSchema from '@typescript-eslint/utils/json-schema';
-export * as TSESLint from '@typescript-eslint/utils/ts-eslint';
-export * from './ts-estree';
-export * as TSUtils from './ts-utils';
