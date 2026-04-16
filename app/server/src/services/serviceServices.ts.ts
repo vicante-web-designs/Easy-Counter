b4a6ef176705c93672ccf75732038f06f98c0022
Batubo Victory <victorybatubo76@gmail.com>
2026-04-16 18:19:47 +0100

    feat: Write all CRUD operations for the service data model

--- app/server/src/services/serviceServices.ts.ts
+++ app/server/src/services/serviceServices.ts.ts
@@ -6,17 +6,30 @@ export async function createService(serviceData: ServiceType) {
     return await supabase.from("services").insert(serviceData).select()
 }
 
-// Get Service
-export async function getService(params:type) {
-    
+// Get All Services
+export async function getAllServices() {
+    const {data, error} = await supabase.from("services").select('*')
+
+    return data
+}
+
+// Get One Services
+export async function getOneService(service_id: string) {
+    const { data, error } = await supabase.from("services").select('*').eq("id", service_id).single()
+
+    if(error) throw error;
 }
 
 // Edit Service
-export async function editService(params:type) {
-    
+export async function updateService(service_id: string, updates: object) {
+    const { data, error } = await supabase.from("services").update(updates).eq("id", service_id).select().single()
+
+    if(error) throw error
 }
 
 // Delete Service
-export async function deleteService(params:type) {
-    
+export async function removeService(service_id: string) {
+    const { data, error } = await supabase.from("services").delete().eq("id", service_id).select().single()
+
+    if(error) throw error
 }
