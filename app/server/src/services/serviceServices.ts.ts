6fcb1169c049025630dce33f7cc178dd96b342b4
Batubo Victory <victorybatubo76@gmail.com>
2026-04-16 17:54:20 +0100

    feat: Create types for each data model

---
+++ app/server/src/services/serviceServices.ts.ts
@@ -0,0 +1,21 @@
+import supabase from '../config/supabase.js';
+
+// Create Service
+export async function createService(serviceData) {
+    return await supabase.from("services").insert(serviceData).select()
+}
+
+// Get Service
+export async function getService(params:type) {
+    
+}
+
+// Edit Service
+export async function editService(params:type) {
+    
+}
+
+// Delete Service
+export async function deleteService(params:type) {
+    
+}
