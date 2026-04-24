1d8f67172d70a78fe98caaa99d48dae41682f42c
Batubo Victory <victorybatubo76@gmail.com>
2026-04-24 18:21:12 +0100

    feat: Import all service CRUD operations

---
+++ app/server/src/tests/service.test.ts
@@ -0,0 +1,5 @@
+import { createService, getAllServices, getOneService, updateService, deleteService } from '../services/service.service.js';
+
+describe('Service CRUD Operations', async () => {
+    
+})
