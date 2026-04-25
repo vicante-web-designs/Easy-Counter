1df3259ce59e6c21f29381e9ccf02454c280e302
Batubo Victory <victorybatubo76@gmail.com>
2026-04-25 16:02:56 +0100

    fix: Make jest compatible with typescript

--- app/server/jest.config.ts
+++ app/server/jest.config.ts
@@ -1,7 +1,15 @@
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
   clearMocks: true,
-};
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.test.json'
+    }]
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+}
