4f17d7a382bbea27b9cd1f72238dc2722006e0f4
Batubo Victory <victorybatubo76@gmail.com>
2026-04-30 16:57:50 +0100

    feat: Add create attendance controller

--- app/server/src/controllers/churchAttendance.controller.ts
+++ app/server/src/controllers/churchAttendance.controller.ts
@@ -3,10 +3,21 @@ import { createChurchAttendance, getAllChurchAttendance, getOneChurchAttendance,
 
 export const handleCreateChurchAttendance = async (req: Request, res: Response) => {
   try {
-    const churchAttendanceData = req.body
-    
-    const data = await createChurchAttendance(churchAttendanceData)
+
+      const churchAttendanceData = req.body
+      
+      const data = await createChurchAttendance(churchAttendanceData)
+
+      return res.status(201).json(data)
+
   } catch (error) {
-    console.error(error)
+
+    if(error instanceof Error){
+
+      return res.status(500).json({ error: error.message })
+
+    }
+
   }
+
 }
