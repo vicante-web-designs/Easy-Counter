ef0fc397bf4cf201f8ffa5a5b0103c0e4479f7aa
Batubo Victory <victorybatubo76@gmail.com>
2026-04-30 15:58:37 +0100

    feat: Write all crud operations for service.controller

--- app/server/src/controllers/service.controller.ts
+++ app/server/src/controllers/service.controller.ts
@@ -1,7 +1,108 @@
+import { Request, Response } from 'express';
 import supabase from '../config/supabase.js';
+import { createService, getAllServices, getOneService, updateService, deleteService } from '../services/service.service.js';
 
-const serviceController = {
+// Handle Create Service
+export const handleCreateService = async (req: Request, res: Response) => {
+  try {
+
+        const serviceData = req.body;
+
+        const data = await createService(serviceData)
+
+        return res.status(201).json(data)
+
+  } catch (error) {
+
+        if(error instanceof Error){
+
+            return res.status(500).json({error: error.message})
+            
+        }
+
+  }
+}
+
+// Handle getAllServices
+export const handleGetAllServices = async (req: Request, res: Response) => {
+  try {
+
+        const data = await getAllServices()
+
+        return res.status(200).json(data)
+
+  } catch (error) {
+
+        if(error instanceof Error){
+
+            return res.status(500).json({error: error.message})
+
+        }
+
+  }
 
 }
 
-export default serviceController
+// Handle getOneService
+export const handleGetOneService = async (req: Request, res: Response) => {
+  try {
+
+        const { id } = req.params
+
+        const data = await getOneService(id as string)
+
+        return res.status(200).json(data)
+
+  } catch (error) {
+
+        if(error instanceof Error){
+
+            return res.status(500).json({error: error.message})
+
+        }
+
+  }
+}
+
+// Handle updateService
+export const handleUpdateService = async (req: Request, res: Response) => {
+  try {
+
+        const { id } = req.params
+        const updatedServiceData = req.body
+
+        const data = await updateService(id as string, updatedServiceData)
+
+        return res.status(200).json(data)
+
+  } catch (error) {
+
+        if(error instanceof Error){
+
+            return res.status(500).json({error: error.message})
+
+        }
+  }
+  
+}
+
+// Handle deleteService
+export const handleDeleteService = async (req: Request, res: Response) => {
+  try {
+
+        const { id } = req.params
+
+        const data = await deleteService(id as string)
+
+        return res.status(200).json(data)
+
+  } catch (error) {
+
+        if(error instanceof Error){
+
+            return res.status(500).json({error: error.message})
+
+        }
+  }
+  
+}
