bdd9e5dc077b05c087eb41695c4d7429932ba1b2
Batubo Victory <victorybatubo76@gmail.com>
2026-05-07 13:16:32 +0100

    feat: Register export csv route

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import churchServiceRouter from './routes/churchService.routes.js'
 import churchAttendanceRouter from './routes/churchAttendance.routes.js'
 import churchSectionRouter from './routes/churchSection.routes.js'
+import csvExportRouter from './routes/csvExport.route.js'
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use(express.json())
 app.use('/api/churchService', churchServiceRouter)
 app.use('/api/churchAttendance', churchAttendanceRouter)
 app.use('/api/churchSection', churchSectionRouter)
+app.use('/api/exportCSV', csvExportRouter)
 
 export default app
