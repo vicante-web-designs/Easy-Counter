ce522fa8ac26e70c60633c84d4ccf92adfb6f896
Batubo Victory <victorybatubo76@gmail.com>
2026-05-09 13:40:10 +0100

    feat: Conditionally render AttendanceForm if IP adress has been submitted

--- app/client/src/pages/counter/CounterPage.tsx
+++ app/client/src/pages/counter/CounterPage.tsx
@@ -1,13 +1,25 @@
-import CountForm from '@/components/CountForm'
+import AttendanceForm from '@/components/features/AttendanceForm'
+import IpForm from '@/components/features/IpForm'
+import { useState } from 'react'
 
 const CounterPage = () => {
-  return (
-    <main>
-      <h1>Service Name</h1>
-      <p>Service Date</p>
-      <p>Live counting session</p>
+  const [isConnected, setIsConnected] = useState<boolean>(false) //TODO: Store isConnected state in redux or localStorage
 
-      <CountForm />
+  return (
+    <main className='w-full max-w-md mx-auto px-4 py-10'>
+      {
+        isConnected ? (
+          <section>
+            <h1>Service Name</h1>
+            <p>Service Date</p>
+            <p>Live counting session</p>
+
+            <AttendanceForm />
+          </section>
+        ) : (
+          <IpForm setIsConnected={setIsConnected}/>
+        )
+      }
     </main>
   )
 }
