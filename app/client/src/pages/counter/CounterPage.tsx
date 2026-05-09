3808057578d50a88a277e4115f49484fb7b08b05
Batubo Victory <victorybatubo76@gmail.com>
2026-05-09 15:40:57 +0100

    refactor: remove Ip form

--- app/client/src/pages/counter/CounterPage.tsx
+++ app/client/src/pages/counter/CounterPage.tsx
@@ -1,14 +1,9 @@
 import AttendanceForm from '@/components/features/AttendanceForm'
-import IpForm from '@/components/features/IpForm'
-import { useState } from 'react'
 
 const CounterPage = () => {
-  const [isConnected, setIsConnected] = useState<boolean>(false) //TODO: Store isConnected state in redux or localStorage
 
   return (
     <main className='w-full max-w-md mx-auto px-4 py-10'>
-      {
-        isConnected ? (
           <section>
             <h1>Service Name</h1>
             <p>Service Date</p>
@@ -16,10 +11,6 @@ const CounterPage = () => {
 
             <AttendanceForm />
           </section>
-        ) : (
-          <IpForm setIsConnected={setIsConnected}/>
-        )
-      }
     </main>
   )
 }
