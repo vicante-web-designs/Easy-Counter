96a712bfefb77f7401a1cb71fc490f0bb1e9cd90
Batubo Victory <victorybatubo76@gmail.com>
2026-05-11 18:08:56 +0100

    fix: Enable cors so client can speak to server

--- app/client/src/lib/api.ts
+++ app/client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getBaseURL = () => {
     const ip = localStorage.getItem('serverIP')
-    return ip ? `http://${ip}:5000` : import.meta.env.VITE_API_URL
+    return ip ? `http://${ip}:8000` : import.meta.env.VITE_API_URL
 }
 export const api = axios.create({
     baseURL: getBaseURL()
