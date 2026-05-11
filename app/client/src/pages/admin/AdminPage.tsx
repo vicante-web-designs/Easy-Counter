4b32a52a07b4aa828c10fec88129187c73c7575b
Batubo Victory <victorybatubo76@gmail.com>
2026-05-11 18:26:38 +0100

    feat: Add create buttons for services and sections

--- app/client/src/pages/admin/AdminPage.tsx
+++ app/client/src/pages/admin/AdminPage.tsx
@@ -2,6 +2,7 @@ import { api } from '@/lib/api'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import AdminDashboard from './AdminDashboard'
+import { Button } from '@/components/ui/button'
 
 const AdminPage = () => {
 
@@ -35,7 +36,7 @@ const AdminPage = () => {
 
   return (
     <main className='w-full h-full flex flex-col items-center gap-6'>
-      <h1 className='text-xl text-center w-fit'>Counters - Open your browsers and go to:</h1>
+      <h1 className='text-xl text-center w-fit'><span className='font-bold'>Counters</span> <br /> Open your browsers and go to:</h1>
 
       <section className='border border-gray-300 bg-gray-200 p-20 rounded-lg'>
         <p className='text-5xl'>
@@ -43,6 +44,11 @@ const AdminPage = () => {
             ip ? `${ip}:${port}` : 'Loading ...'
           }
         </p>
+      </section>
+
+      <section className='flex gap-4'>
+        <Button>Create Service</Button>
+        <Button>Create Section</Button>
       </section>
 
       <AdminDashboard />
