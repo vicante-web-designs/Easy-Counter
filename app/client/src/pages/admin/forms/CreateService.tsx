9fefc678ee2386d754a93f0988d1d6bb499c8b72
Batubo Victory <victorybatubo76@gmail.com>
2026-05-12 14:20:17 +0100

    feat: Add content on createService page

--- app/client/src/pages/admin/forms/CreateService.tsx
+++ app/client/src/pages/admin/forms/CreateService.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router'
+import { Button } from '@/components/ui/button'
+import ServiceForm from '@/components/features/ServicesForm'
+
 const CreateServicePage = () => {
+  const navigate = useNavigate()
 
   return (
-    <main className='w-full max-w-md mx-auto px-4 py-10'>
-          <section>
-            <h1>Service Name</h1>
-            <p>Service Date</p>
-            <p>Live counting session</p>
+    <main className='w-full mx-auto px-4 py-10 flex flex-col gap-8 h-full items-center'>
+          <section className='flex flex-col text-center gap-4 max-w-md'>
+            <h1 className='text-4xl font-bold'>Create new Service</h1>
+            <p>Fill in the form below to create a new service</p>
           </section>
+
+          <Button className='w-fit' onClick={() => navigate('/adminPage')}>Return to Dashboard</Button>
+
+          <ServiceForm />
     </main>
   )
 }
